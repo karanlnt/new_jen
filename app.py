ff3ac5e01d7d4b7f78d049afb77c8701ebdd314d
karanlnt <100667916+karanlnt@users.noreply.github.com>
2025-08-05 11:00:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify(message="Hello from Flask on Docker!")
+    return jsonify(message="Hello from Flask on Docker kutty !")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000)
